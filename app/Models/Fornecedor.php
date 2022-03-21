2201c4b55319f8d10d6b2d8b60a0a02c9cf67867
Kawan Araújo <kawanaraujocontato@gmail.com>
2022-03-21 07:47:00 -0300

    Inserindo dados no banco de dados

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     use HasFactory;
+    protected $table = 'fornecedores';//Sobrepondo o atributo definido automaticamente pelo model.
+    protected $fillable = ['nome','site', 'email', 'uf'];
+
+    /* Inserindo dados usando uma instância: */
+    /* $f = new App\Models\Fornecedor();
+    $f->nome = 'kawan';
+    $f->site = 'kawan.com';
+    $f->email = 'contato@kawan.com';
+    $f->uf = 'SP';
+    $f->getAttributes();
+    $f->save(); */
+
+    /* Inserindo d ados usando método estatico */
+    /* App\Models\Fornecedor::create([
+        'nome'->'kawan',
+        'site'->'kawan.com',
+        'email'->'contato@kawan.com',
+        'uf'->'SP'
+    ]); */
 }
